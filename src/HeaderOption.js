253adbe6c234221b73b856d7bdb5dd091171f517
Harsh-Botmantra <harsh.upadhyay@botmantra.com>
2023-10-16 12:04:42 +0530

    new changes

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,52 +1,67 @@
-import React from 'react';
-import './HeaderOption.css';
-import Avatar from '@mui/material/Avatar';
-import { LogOut } from './redux/action';
-import { useDispatch , useSelector } from 'react-redux';
-import baseurl from './Axios';
-import { useNavigate } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import React from "react";
+import "./HeaderOption.css";
+import Avatar from "@mui/material/Avatar";
+import { LogOut } from "./redux/action";
+import { useDispatch, useSelector } from "react-redux";
+import baseurl from "./Axios";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
-
-export default function HeaderOption({avatr , Icons , title}) {
+export default function HeaderOption({ avatr, Icons, title }) {
   const dispatch = useDispatch();
   const history = useNavigate();
-  const value = useSelector((state)=> state);
+  const [displayHidden, setdisplayHidden] = React.useState("none");
 
-  
+  const value = useSelector((state) => state);
+
   const handleLogoutFunction = () => {
-    
-    baseurl.post(`http://localhost:5001/api/logout` , {token:localStorage.getItem("token")})
-    .then( (res) => {
-        if(res.data.status === true) {
-          dispatch(LogOut()); 
+    baseurl
+      .post(`http://localhost:5001/api/logout`, {
+        token: localStorage.getItem("token"),
+      })
+      .then((res) => {
+        if (res.data.status === true) {
+          dispatch(LogOut());
           history("/");
           localStorage.removeItem("token");
           localStorage.removeItem("name");
           alert(res.data.message);
         } else {
           alert(res.data.message);
-        }  
-    } )
-    .catch( (err) => {
-      toast.error(err.response.data.message, {
-        autoClose: 5000,
+        }
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message, {
+          autoClose: 5000,
         });
-    } )
-  }
+      });
+  };
 
   return (
-    <> <ToastContainer />
-    <div className='headerOption'>
-    {avatr  && <div className="AvtarMainDiv"> <div className="avtar"> 
-        <span className="spanTag" onClick={handleLogoutFunction}> 
-        Logout </span>  
-        </div> <Avatar className="headerOption_icons" src={value.url}/> 
-        </div>  }
-    {Icons && <Icons className="headerOption_icons"/>}
-    <h3 className="headerOption_title">{title}</h3>
-    </div>
+    <>
+      {" "}
+      <ToastContainer />
+      <div className="headerOption">
+        {avatr && (
+          <div className="AvtarMainDiv" 
+          onMouseEnter={() => setdisplayHidden("block")}
+          onMouseLeave={() => setdisplayHidden("none")}
+          >
+            <div className="avtar" style={{display: displayHidden}}>
+              <span className="spanTag" onClick={handleLogoutFunction}>
+                Logout
+              </span>
+            </div>
+            <Avatar className="headerOption_icons" src={value.url} />
+          </div>
+        )}
+        {Icons && <Icons className="headerOption_icons" />}
+        <h3 className="headerOption_title" 
+         onMouseEnter={() => setdisplayHidden("block")}
+         onMouseLeave={() => setdisplayHidden("none")}
+        >{title}</h3>
+      </div>
     </>
-  )
+  );
 }
 //harsh
--- src/Post.js
+++ src/Post.js
@@ -9,51 +9,63 @@ import ShareIcon from "@mui/icons-material/Share";
 import { useSelector } from "react-redux";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
-const Post = forwardRef(({ name, description, message, image ,id ,handleDelete , avatar }, ref) => {
-  const [colorForLikeButton, setColorForLikeButton] = useState(false);
-  const values = useSelector((state) => state);
-  const [triggerForDeleteIcons , setTriggerForDeleteIcons] = useState(false);
+const Post = forwardRef(
+  ({ name, description, message, image, id, handleDelete, avatar }, ref) => {
+    const [colorForLikeButton, setColorForLikeButton] = useState(false);
+    const values = useSelector((state) => state);
+    const [triggerForDeleteIcons, setTriggerForDeleteIcons] = useState(false);
 
-  const handleClickForLike = () => {
-    setColorForLikeButton(!colorForLikeButton);
-  };
+    const handleClickForLike = () => {
+      setColorForLikeButton(!colorForLikeButton);
+    };
 
-  return (
-    <div ref={ref} className="post">
-      <div className="postleader">
-        <Avatar src={avatar} />
-        <div className="post_info">
-          <h2>{name}</h2>
-          <p>{description}</p>
-        </div>
-
-        <div className="MoreVertIcon">
-          <MoreVertIcon onClick={()=> setTriggerForDeleteIcons(!triggerForDeleteIcons)} />
-          <div onClick={()=> handleDelete(id)} style={{display: triggerForDeleteIcons ? "block" : "none"}} className="MoreVertical-delete" >
-              Delete Post
+    return (
+      <div ref={ref} className="post">
+        <div className="postleader">
+          <Avatar src={avatar} />
+          <div className="post_info">
+            <h2>{name}</h2>
+            <p>{description}</p>
           </div>
+
+          {localStorage.getItem("name") === name && (
+            <div className="MoreVertIcon">
+              <MoreVertIcon
+                onClick={() => setTriggerForDeleteIcons(!triggerForDeleteIcons)}
+              />
+              <div
+                onClick={() => handleDelete(id)}
+                style={{ display: triggerForDeleteIcons ? "block" : "none" }}
+                className="MoreVertical-delete"
+              >
+                Delete Post
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="postBody">
+          <p>{message}</p>
+        </div>
+
+        <div className="imageForDisplay">
+          {image && <img src={image} width="100%" />}
+        </div>
+
+        <div className="post_button">
+          <InputOption
+            Icons={colorForLikeButton ? ThumbDownAltIcon : ThumbUpIcon}
+            title="Like"
+            color={colorForLikeButton ? "black" : "#0a66c2"}
+            handleClick={handleClickForLike}
+          />
+          <InputOption Icons={CommentIcon} title="comment" color="gray" />
+          <InputOption Icons={ShareIcon} title="share" color="gray" />
+          {/* <InputOption Icons={SendIcon} title="send" color="gray" />  */}
         </div>
       </div>
-
-      <div className="postBody">
-        <p>{message}</p>
-      </div>
-
-      <div className="imageForDisplay">{image && <img src={image} width="100%" />}</div>
-
-      <div className="post_button">
-        <InputOption
-          Icons={colorForLikeButton ? ThumbDownAltIcon : ThumbUpIcon}
-          title="Like"
-          color={colorForLikeButton ? "black" : "#0a66c2"}
-          handleClick={handleClickForLike}
-        />
-        <InputOption Icons={CommentIcon} title="comment" color="gray" />
-        <InputOption Icons={ShareIcon} title="share" color="gray" />
-        {/* <InputOption Icons={SendIcon} title="send" color="gray" />  */}
-      </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Post;
